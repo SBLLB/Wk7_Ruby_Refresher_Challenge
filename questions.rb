c1100f79f33eb47e4f6a9ceef19c71e2d2a16f95
RachelNolan <mail@rachelnolan.com>
2014-11-02 14:06:11 +0000

    Questions 1 & 2

--- questions.rb
+++ questions.rb
@@ -1,13 +1,16 @@
 # keep only the elements that start with an a
 def select_elements_starting_with_a(array)
+	array.select {|element| element.chr == "a"}
 end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+	array.select {|element| element.chr =~ /[aeiou]/}
 end
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(array)
+	# array.delete_if { |a| a = nil }
 end
 
 # remove instances of nil AND false from an array
